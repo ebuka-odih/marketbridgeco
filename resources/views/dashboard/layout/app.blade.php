9edd86a6cbf666b4dbb7d429c2261e6d9fb8b3b4
Gnosis <emmagab38@gmail.com>
2023-06-14 02:01:22 +0100

    added livechat

--- resources/views/dashboard/layout/app.blade.php
+++ resources/views/dashboard/layout/app.blade.php
@@ -92,7 +92,9 @@
         }
     </style>
 
-    <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="db61b6b0-8bf7-4e1f-aa61-c4b8f58cdb9b";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+
+    <script src="//code.jivosite.com/widget/XedzK1NI6p" async></script>
+
 </head>
 <body class="dark-mode">
 
